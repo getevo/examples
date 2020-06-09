630ab68d80e260ca33166c368cc2da13d4b6a809
Reza <reza@ies-italia.it>
2020-06-09 19:39:10 +0200

    bible

--- dev/main.go
+++ dev/main.go
@@ -5,7 +5,6 @@ import (
 	"github.com/getevo/evo/apps/admin"
 	"github.com/getevo/evo/apps/adminlte"
 	"github.com/getevo/evo/apps/auth"
-	"github.com/getevo/evo/apps/bible"
 	"github.com/getevo/evo/apps/query"
 	"github.com/getevo/evo/apps/test"
 	"github.com/getevo/examples/strange"
@@ -20,6 +19,5 @@ func main() {
 	auth.Register()
 	query.Register()
 	test.Register()
-	bible.Register()
 	evo.Run()
 }
