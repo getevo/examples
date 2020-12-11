e6bf5f95e340957d334370282593f2b55edebbf6
allan <allan.nava@ies-italia.it>
2020-12-11 20:26:55 +0100

    update

---
+++ http_example/main.go
@@ -0,0 +1,21 @@
+package main
+
+///
+import (
+	"github.com/getevo/evo"
+	"github.com/getevo/evo/apps/rdb"
+)
+
+//
+func main() {
+	evo.Setup()
+	rdb.Register()
+	evo.Get("/", func(r *evo.Request) {
+		r.WriteResponse("data")
+	})
+	/*func(r *evo.Request) {
+		fmt.Println("http Registered")
+		r.WriteResponse("Home")
+	})*/
+	evo.Run()
+}
